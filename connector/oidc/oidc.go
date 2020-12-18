0ac0748254a8424017de3f259e0d78af64c84b0e
Manuel Ganter <manuel.ganter@hotmail.de>
2020-12-18 13:37:48 +0100

    Added fix for Mail

--- connector/oidc/oidc.go
+++ connector/oidc/oidc.go
@@ -307,11 +307,13 @@ func (c *oidcConnector) createIdentity(ctx context.Context, identity connector.I
 	if !found {
 		return identity, fmt.Errorf("missing \"%s\" claim", userNameKey)
 	}
+	name = strings.ToLower(name)
 
 	preferredUsername, found := claims["preferred_username"].(string)
 	if (!found || c.overrideClaimMapping) && c.preferredUsernameKey != "" {
 		preferredUsername, _ = claims[c.preferredUsernameKey].(string)
 	}
+	preferredUsername = strings.ToLower(preferredUsername)
 
 	hasEmailScope := false
 	for _, s := range c.oauth2Config.Scopes {
@@ -328,6 +330,7 @@ func (c *oidcConnector) createIdentity(ctx context.Context, identity connector.I
 		emailKey = c.emailKey
 		email, found = claims[emailKey].(string)
 	}
+	email = strings.ToLower(email)
 
 	if !found && hasEmailScope {
 		return identity, fmt.Errorf("missing email claim, not found \"%s\" key", emailKey)
